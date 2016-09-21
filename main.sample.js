f2aa32747cb777436dc2954040128096f8d7df1e
spyingwind <spyingwind@gmail.com>
2016-09-21 17:11:47 -0500

    Update sample main.js

--- main.sample.js
+++ main.sample.js
@@ -1,5 +1,3 @@
-
-
 var game = require('prototype.game'); // This is about the only way I could get this to work.
 require('prototype.room')();
 require('prototype.spawn')();
@@ -8,27 +6,32 @@ require('prototype.creep')();
 
 module.exports.loop = function () {
 
+    game.setup();
 
-//getHarvestedEnergy and getUsedEnergy example usage.  
+    //getHarvestedEnergy and getUsedEnergy example usage.
+    let rooms = Game.rooms;
     for (let roomKey in rooms) {
-      let room = Game.rooms[roomKey];
-      var isMyRoom = (room.controller ? room.controller.my : 0);
-      if (isMyRoom) {
-          let deposit = room.getHarvestedEnergy();
-          if(deposit){
-              Memory.stats['room.' + room.name + '.homeHarvestedEnergy'] = deposit;
-              Memory.stats['room.' + room.name + '.homeHarvestedEnergyTotal'] = Memory.stats['room.' + room.name + '.homeHarvestedEnergyTotal'] + Memory.stats['room.' + room.name + '.homeHarvestedEnergy'];
-          }else{
-              Memory.stats['room.' + room.name + '.homeHarvestedEnergy'] = 0;
-          }
-          let withdraw = room.getUsedEnergy();
-          if(withdraw){
-              Memory.stats['room.' + room.name + '.homeUsedEnergy'] = withdraw;
-              Memory.stats['room.' + room.name + '.homeUsedEnergyTotal'] = Memory.stats['room.' + room.name + '.homeUsedEnergyTotal'] + Memory.stats['room.' + room.name + '.homeUsedEnergy'];
-          }else{
-              Memory.stats['room.' + room.name + '.homeUsedEnergy'] = 0;
-          }
-          
-      }
-  }
+        if (!rooms.hasOwnProperty(roomKey)) {
+            continue;
+        }
+        let room = Game.rooms[roomKey];
+        var isMyRoom = (room.controller ? room.controller.my : 0);
+        if (isMyRoom) {
+            let deposit = room.getHarvestedEnergy();
+            if (deposit) {
+                Memory.stats['room.' + room.name + '.homeHarvestedEnergy'] = deposit;
+                Memory.stats['room.' + room.name + '.homeHarvestedEnergyTotal'] = Memory.stats['room.' + room.name + '.homeHarvestedEnergyTotal'] + Memory.stats['room.' + room.name + '.homeHarvestedEnergy'];
+            } else {
+                Memory.stats['room.' + room.name + '.homeHarvestedEnergy'] = 0;
+            }
+            let withdraw = room.getUsedEnergy();
+            if (withdraw) {
+                Memory.stats['room.' + room.name + '.homeUsedEnergy'] = withdraw;
+                Memory.stats['room.' + room.name + '.homeUsedEnergyTotal'] = Memory.stats['room.' + room.name + '.homeUsedEnergyTotal'] + Memory.stats['room.' + room.name + '.homeUsedEnergy'];
+            } else {
+                Memory.stats['room.' + room.name + '.homeUsedEnergy'] = 0;
+            }
+
+        }
+    }
 }
